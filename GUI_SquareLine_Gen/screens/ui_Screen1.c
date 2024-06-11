6dadfa2c9a83b2b046e3f778344745305936ab33
sudoer <hhj@sudoer.cn>
2024-06-11 22:19:14 +0800

    启用18号字体，修改键盘字体大小

--- GUI_SquareLine_Gen/screens/ui_Screen1.c
+++ GUI_SquareLine_Gen/screens/ui_Screen1.c
@@ -37,6 +37,7 @@ void ui_Screen1_screen_init(void)
     lv_obj_set_style_bg_opa(ui_Screen1_Keyboard_Keyboard1, 255, LV_PART_ITEMS | LV_STATE_DEFAULT);
     lv_obj_set_style_text_color(ui_Screen1_Keyboard_Keyboard1, lv_color_hex(0x000000), LV_PART_ITEMS | LV_STATE_DEFAULT);
     lv_obj_set_style_text_opa(ui_Screen1_Keyboard_Keyboard1, 255, LV_PART_ITEMS | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Screen1_Keyboard_Keyboard1, &lv_font_montserrat_18, LV_PART_ITEMS | LV_STATE_DEFAULT);
 
     ui_Screen1_Label_Label3 = lv_label_create(ui_Screen1);
     lv_obj_set_width(ui_Screen1_Label_Label3, LV_SIZE_CONTENT);   /// 1
